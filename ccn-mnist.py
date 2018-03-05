06313f0f93d420b3b78441a023511d0296ebcdc1
Jeetu singh <xtaraim@gmail.com>
2018-03-05 15:53:55 +0530

    Create ccn-mnist.py

---
+++ ccn-mnist.py
@@ -0,0 +1,12 @@
+ from __future__ import absolute_import
+>>> from __future__ import division
+>>> from __future__ import print_function
+>>> #imports
+>>> import numpy as np
+>>> import tensorflow as tf
+tf.logging.se
+>>> tf.logging.set_verbosity(tf .logging.INFO)
+>>> # Our application logic will be added here
+>>> if __name__ == "__main__":
+	tf.app.run()
+	
